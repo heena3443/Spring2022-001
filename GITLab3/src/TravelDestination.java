e64575d6f101815fc017afc42b2f5ebc04a30fb8
heena3443 <61155560+heena3443@users.noreply.github.com>
2022-02-14 17:03:22 -0600

    Update TravelDestination.java

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -2,7 +2,7 @@
 public class TravelDestination {
 
 	public static void main(String[] args) {
-				System.out.println("My name is Heena Rathore. I wish to travel to Hawai one day !");
+				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
 
 	}
 
