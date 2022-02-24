e2e2ebeef145472db28664b49153dfdf979104e9
martinsanabia <martin.sanabia@my.utsa.edu>
2022-02-24 01:17:33 -0600

    This is my first commit edit

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -3,7 +3,7 @@ public class TravelDestination {
 
 	public static void main(String[] args) {
 				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
-
+				System.out.println("My name is Martin Sanabia. I wish to travel to Canada one day");
 	}
 
 }
