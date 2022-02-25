bfdc4384c0cefc29f8b76100b95515b706092325
GioTeran <99766706+GioTeran@users.noreply.github.com>
2022-02-25 16:18:55 -0600

    Added a print statement
    The print statement prints out where I would like to travel one day.

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -4,6 +4,7 @@ public class TravelDestination {
 	public static void main(String[] args) {
 				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
 				System.out.println("My name is Martin Sanabia. I wish to travel to Canada one day");
+				System.out.println("My name is Giovanni Teran. I wish to travel to South Korea one day !");
 	}
 
 }
