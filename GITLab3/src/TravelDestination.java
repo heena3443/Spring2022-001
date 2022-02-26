43614fee31f7e6a0207037b269c96a947b101c93
its-ric0 <99773798+its-ric0@users.noreply.github.com>
2022-02-26 17:28:58 -0600

    update TravelDestination.java
    My commit to TravelDestination.java file

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -5,6 +5,7 @@ public class TravelDestination {
 				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
 				System.out.println("My name is Martin Sanabia. I wish to travel to Canada one day");
 				System.out.println("My name is Giovanni Teran. I wish to travel to South Korea one day!");
+				System.out.println("My name is David Rico. I wish to travel to Japan one day!");
 	}
 
 }
