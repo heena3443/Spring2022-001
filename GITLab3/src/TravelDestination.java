20954cf442f0de3707a8f9af266867b8b296e85c
Jhan Baldovino <j.baldovino190@gmail.com>
2022-02-28 20:49:06 -0600

    Jhan Baldovino added a print statement

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -11,7 +11,7 @@ public class TravelDestination {
 				System.out.println("My name is Jose Daniel Gutierrez. I wish to traver to Argentina one day!");
 				System.out.println("My name is Arnold Lee. I wish to travel to Japan one day!");
 				System.out.println("My name is Joshua Navarro. I wish to travel to Japan one day!");
-				
+				System.out.println("My name is Jhan Baldovino. I wish to travel to Rome one day!");
 	}
 
 }
