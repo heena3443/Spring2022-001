c95a06234f2ff6fcb09ee13eba4fd81f24a75ff9
josh <joshua.navarro729@gmail.com>
2022-02-28 19:18:11 -0600

    Updated TravelDestination.java

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -10,6 +10,8 @@ public class TravelDestination {
 				System.out.println("My names is James Pachikara. I wish tro tracel to Germany one day!");
 				System.out.println("My name is Jose Daniel Gutierrez. I wish to traver to Argentina one day!");
 				System.out.println("My name is Arnold Lee. I wish to travel to Japan one day!");
+				System.out.println("My name is Joshua Navarro. I wish to travel to Japan one day!");
+				
 	}
 
 }
