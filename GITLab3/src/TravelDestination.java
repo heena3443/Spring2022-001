924497567ba3b7f88c3a0e52fe542d5fa3b786d1
LoganHall195 <99156058+LoganHall195@users.noreply.github.com>
2022-03-01 18:21:45 -0600

    Update TravelDestination.java

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -21,6 +21,7 @@ public class TravelDestination {
 				System.out.println("My name is Nicholas Breach. I wish to travel to Canada one day!");
                                 System.out.println("My name is Alex Rodriguez. I wish to travel to Quebec one day!"); 
 				System.out.println("My name is Roshan Vareed. I wish to travel to Quebec one day!"); 
+				System.out.println("My name is Logan Hall. I wish to travel to New York one day!"); 
 
 
 	}
