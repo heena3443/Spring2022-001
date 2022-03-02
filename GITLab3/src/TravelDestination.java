a94d9537d32e2c04313097cdf31e8b8e0ba233bf
acrviper21 <ecrash789@gmail.com>
2022-03-01 20:56:19 -0600

    Updating TravelDestination.Java

--- GITLab3/src/TravelDestination.java
+++ GITLab3/src/TravelDestination.java
@@ -27,7 +27,7 @@ public class TravelDestination {
         System.out.println("My name is Logan Hall. I wish to travel to New York one day!"); 
 				System.out.println("My name is Marco De Luna. I wish to travel to Canada one day!");
 				System.out.println("My name is Bernardo Vazquez De La Cruz. I wish to travel to Prague one day!");
-				System.out.println("My name is Erik Sistos. I wish to travel to Italy one day!);
+				System.out.println("My name is Erik Sistos. I wish to travel to Italy one day!");
 	}
 
 }
